93660d397bb7c6b3732a5f8f15784fa9c3947ddc
huseyinbabal <huseyinbabal88@gmail.com>
2022-12-04 00:50:18 +0300

    final

--- main.tf
+++ main.tf
@@ -6,3 +6,25 @@ terraform {
     }
   }
 }
+
+provider "google" {
+  project = "protocloud-368418"
+}
+
+resource "google_cloud_run_service" "ping" {
+  name     = "ping"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "ghcr.io/huseyinbabal/ping:main"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
